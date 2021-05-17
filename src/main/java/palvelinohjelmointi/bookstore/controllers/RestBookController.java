ebed089b3bb4b70614e27981cd985c2f6d915886
niikari <pakniiles@gmail.com>
2021-05-17 10:26:23 +0300

    REST API 2

--- src/main/java/palvelinohjelmointi/bookstore/controllers/RestBookController.java
+++ src/main/java/palvelinohjelmointi/bookstore/controllers/RestBookController.java
@@ -1,10 +1,13 @@
 package palvelinohjelmointi.bookstore.controllers;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import palvelinohjelmointi.bookstore.models.Book;
@@ -16,10 +19,7 @@ public class RestBookController {
 	
 	@Autowired
 	private BookRepository bookRepository;
-	
-	@Autowired
-	private CategoryRepository categoryRepository;
-	
+		
 	@GetMapping("/books")
 	public Iterable<Book> returnBooks() {
 		return this.bookRepository.findAll();
@@ -29,5 +29,17 @@ public class RestBookController {
 	public Optional<Book> returnBookById(@PathVariable(value="id") Long id) {
 		
 		return this.bookRepository.findById(id);
+	}
+	
+	@GetMapping("/books/{author}")
+	public List<Book> returnBookByAuthor(@PathVariable(value="author") String author) {
+		
+		return this.bookRepository.findByAuthor(author);
+	}
+	
+	@PostMapping("/books")
+	public Book addNewBook(@RequestBody Book book) {
+		
+		return this.bookRepository.save(book);
 	}
 }
--- src/main/java/palvelinohjelmointi/bookstore/repositories/BookRepository.java
+++ src/main/java/palvelinohjelmointi/bookstore/repositories/BookRepository.java
@@ -3,9 +3,12 @@ package palvelinohjelmointi.bookstore.repositories;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import palvelinohjelmointi.bookstore.models.Book;
 
+@RepositoryRestResource
 public interface BookRepository extends CrudRepository<Book, Long> {
-	List<Book> findByAuthor(String author);
+	List<Book> findByAuthor(@Param("author") String author);
 }
