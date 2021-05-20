b140c7db45d0d26826b69c34be48fe35928901d2
niikari <pakniiles@gmail.com>
2021-05-20 09:38:25 +0300

    java version vaihdettu 11 => 1.8

--- src/test/java/palvelinohjelmointi/bookstore/BookstoreApplicationTests.java
+++ src/test/java/palvelinohjelmointi/bookstore/BookstoreApplicationTests.java
@@ -3,7 +3,6 @@ package palvelinohjelmointi.bookstore;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
